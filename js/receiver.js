865f0ee8a51a8b5431492b5c2ed64da6d5575f98
German <germancandradem@gmail.com>
2021-02-07 00:50:04 -0500

    disabled debug

--- js/receiver.js
+++ js/receiver.js
@@ -2,25 +2,20 @@ const context = cast.framework.CastReceiverContext.getInstance();
 const playerManager = context.getPlayerManager();
 
 // Debug Logger
-const castDebugLogger = cast.debug.CastDebugLogger.getInstance();
+// const castDebugLogger = cast.debug.CastDebugLogger.getInstance();
 const LOG_TAG = 'MyAPP.LOG';
 
 // Enable debug logger and show a 'DEBUG MODE' overlay at top left corner.
-castDebugLogger.setEnabled(true);
+// castDebugLogger.setEnabled(true);
 
 // Show debug overlay
 // castDebugLogger.showDebugLogs(true);
 
 // Set verbosity level for Core events.
-castDebugLogger.loggerLevelByEvents = {
-  'cast.framework.events.category.CORE': cast.framework.LoggerLevel.INFO,
-  'cast.framework.events.EventType.MEDIA_STATUS': cast.framework.LoggerLevel.DEBUG
-}
+//castDebugLogger.loggerLevelByEvents = {'cast.framework.events.category.CORE': cast.framework.LoggerLevel.INFO,'cast.framework.events.EventType.MEDIA_STATUS': cast.framework.LoggerLevel.DEBUG}
 
 // Set verbosity level for custom tags.
-castDebugLogger.loggerLevelByTags = {
-    LOG_TAG: cast.framework.LoggerLevel.DEBUG,
-};
+// castDebugLogger.loggerLevelByTags = {LOG_TAG: cast.framework.LoggerLevel.DEBUG};
 
 function makeRequest (url,payload) {
 	var xhr = new XMLHttpRequest();
