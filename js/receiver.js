8e902409b7fe4a7f6e9678b247d4c1fa3734fa1e
gcandrade10 <gc.andrade10@uniandes.edu.co>
2021-05-19 14:06:15 -0500

    Update receiver.js

--- js/receiver.js
+++ js/receiver.js
@@ -6,16 +6,16 @@ const playerManager = context.getPlayerManager();
 const LOG_TAG = 'MyAPP.LOG';
 
 // Enable debug logger and show a 'DEBUG MODE' overlay at top left corner.
-// castDebugLogger.setEnabled(true);
+castDebugLogger.setEnabled(true);
 
 // Show debug overlay
-// castDebugLogger.showDebugLogs(true);
+castDebugLogger.showDebugLogs(true);
 
 // Set verbosity level for Core events.
-//castDebugLogger.loggerLevelByEvents = {'cast.framework.events.category.CORE': cast.framework.LoggerLevel.INFO,'cast.framework.events.EventType.MEDIA_STATUS': cast.framework.LoggerLevel.DEBUG}
+castDebugLogger.loggerLevelByEvents = {'cast.framework.events.category.CORE': cast.framework.LoggerLevel.INFO,'cast.framework.events.EventType.MEDIA_STATUS': cast.framework.LoggerLevel.DEBUG}
 
 // Set verbosity level for custom tags.
-// castDebugLogger.loggerLevelByTags = {LOG_TAG: cast.framework.LoggerLevel.DEBUG};
+castDebugLogger.loggerLevelByTags = {LOG_TAG: cast.framework.LoggerLevel.DEBUG};
 
 function makeRequest (url,payload) {
 	var xhr = new XMLHttpRequest();
