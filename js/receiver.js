f15f2403ac8945d6ad1921808553ea5d72490579
gcandrade10 <gc.andrade10@uniandes.edu.co>
2021-05-19 14:10:55 -0500

    Update receiver.js

--- js/receiver.js
+++ js/receiver.js
@@ -2,7 +2,7 @@ const context = cast.framework.CastReceiverContext.getInstance();
 const playerManager = context.getPlayerManager();
 
 // Debug Logger
-// const castDebugLogger = cast.debug.CastDebugLogger.getInstance();
+const castDebugLogger = cast.debug.CastDebugLogger.getInstance();
 const LOG_TAG = 'MyAPP.LOG';
 
 // Enable debug logger and show a 'DEBUG MODE' overlay at top left corner.
