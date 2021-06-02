05905828fa928c0fc11541a6144fa44db64537ac
gcandrade10 <gc.andrade10@uniandes.edu.co>
2021-06-01 23:28:47 -0500

    Update receiver.js

--- js/receiver.js
+++ js/receiver.js
@@ -6,10 +6,10 @@ const castDebugLogger = cast.debug.CastDebugLogger.getInstance();
 const LOG_TAG = 'MyAPP.LOG';
 
 // Enable debug logger and show a 'DEBUG MODE' overlay at top left corner.
-castDebugLogger.setEnabled(true);
+castDebugLogger.setEnabled(false);
 
 // Show debug overlay
-castDebugLogger.showDebugLogs(true);
+castDebugLogger.showDebugLogs(false);
 
 // Set verbosity level for Core events.
 castDebugLogger.loggerLevelByEvents = {'cast.framework.events.category.CORE': cast.framework.LoggerLevel.INFO,'cast.framework.events.EventType.MEDIA_STATUS': cast.framework.LoggerLevel.DEBUG}
